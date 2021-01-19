00ec677b9b087339ae1d2a50d66c91197200bf11
Mohit Sethi <mohitsethi.32@gmail.com>
2021-01-19 07:05:24 +0530

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"include/**.cpp",
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
